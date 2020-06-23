3c06c2c7848628424864041f99794c0e03d5dd63
jay <jay0764@naver.com>
2020-06-23 14:53:20 +0900

    finish

---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
+function LoginPage(props) {
+	const dispatch = useDispatch();
+	const [Email, setEmail] = useState('');
+	const [Password, setPassword] = useState('');
+	const onEmailHandler = (event) => {
+		setEmail(event.currentTarget.value);
+	};
+	const onPasswordHandler = (event) => {
+		setPassword(event.currentTarget.value);
+	};
+	const onSubmitHandler = (event) => {
+		event.preventDefault(); //page가 refresh 되는 것을 막아줌
+		let body = {
+			email: Email,
+			password: Password,
+		};
+		dispatch(loginUser(body)).then((response) => {
+			if (response.payload.loginSuccess) {
+				props.history.push('/'); //react에서 페이지를 이동시키는 방식
+			} else {
+				alert('Error');
+			}
+		});
+	};
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				width: '100%',
+				height: '100vh',
+			}}
+		>
+			<form
+				style={{ display: 'flex', flexDirection: 'column' }}
+				onSubmit={onSubmitHandler}
+			>
+				<label>Email</label>
+				<input type="email" value={Email} onChange={onEmailHandler} />
+				<label>Password</label>
+				<input type="password" value={Password} onChange={onPasswordHandler} />
+				<br />
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
+}
+
+export default withRouter(LoginPage);
